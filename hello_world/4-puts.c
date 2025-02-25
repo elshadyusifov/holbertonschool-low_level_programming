eb3b1fe14b663139981af7d16b4c3cd22c67af91
elshadyusifov <e.yusifov335@gmail.com>
2025-02-25 21:23:47 +0400

    create C file with puts command inbetty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * main puts command C
  */
 int main(void)
 {
-char line = ("\"Programming is like building a multilingual puzzle\n");
-puts(char line);
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }

23bae0b0b05c076b0c049c28873267e3727825fb
elshadyusifov <e.yusifov335@gmail.com>
2025-02-25 21:47:36 +0400

    create C file with puts command inbetty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- * main puts command C
+ * main - puts command C
+ *
+ * Description: This program prints a statement about programming,
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/*
- * yazdir
- */
-	return (0);
+return (0);
 }

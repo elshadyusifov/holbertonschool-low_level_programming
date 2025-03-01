b81609819a66dc99ffabbbde13cb36afa028d238
elshadyusifov <e.yusifov335@gmail.com>
2025-03-01 16:20:30 +0400

    the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Always (0) success
+ */
+int main(void)
+{
+char balaca_herfler = 'a';
+for (; balaca_herfler <= 'z'; balaca_herfler++)
+{
+putchar(balaca_herfler);
+}
+putchar('\n');
+return (0);
+}

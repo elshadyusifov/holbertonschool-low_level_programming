745da6b1d3127be46cedefaaf53f42072512f492
elshadyusifov <e.yusifov335@gmail.com>
2025-03-01 17:43:17 +0400

    all single digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - number 0 to 9
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+int i = 0;
+for (; i <= 9 ; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}

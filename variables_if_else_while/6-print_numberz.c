0de2e53fff92b3380e322769133e2293a11bed96
elshadyusifov <e.yusifov335@gmail.com>
2025-03-01 18:00:42 +0400

    digit numbers of base 10 starting from 0 like character

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - number 0 to 9
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+int i = 48;
+for (; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

95109f6ae8cb32035a8a1af061a8da05fab02d8a
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 20:32:48 +0400

    prints the numbers, from 0 to 9, followed by a new line without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
+ *
+ * Description: This function prints all the numbers from 0 to 9 and
+ * then prints a new line.
+ */
+
+void print_numbers(void)
+{
+char numbers[] = "0123456789";
+int i = 0;
+while (numbers[i] != '\0')
+if (numbers[3] != 2 && numbers[5] != 4)
+{
+_putchar(numbers[i]);
+i++;
+}
+_putchar('\n');
+}

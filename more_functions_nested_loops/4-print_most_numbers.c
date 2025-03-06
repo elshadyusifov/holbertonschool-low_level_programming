c15998a150ce1558f18eb25021d494a377fc566f
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 20:54:27 +0400

    I believe in numbers and signs without 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
+ * print_most_numbers - Prints the numbers from 0 to 9 followed by a new line.
  *
  * Description: This function prints all the numbers from 0 to 9 and
  * then prints a new line.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 char numbers[] = "0123456789";
 int i = 0;

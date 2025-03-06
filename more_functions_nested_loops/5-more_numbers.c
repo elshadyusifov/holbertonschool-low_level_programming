556da9ee4973c93e6853efd23f29c2786809dd02
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 21:43:02 +0400

    prints 10 times the numbers, from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
-if (j>9)
-_putchar(j / 10 + '0');
+if (j > 9)
+_putchar((j / 10) + '0');
 }
-_putchar(i % 10 + '0');
+_putchar((i % 10) + '0');
 _putchar('\n');
 }
 }

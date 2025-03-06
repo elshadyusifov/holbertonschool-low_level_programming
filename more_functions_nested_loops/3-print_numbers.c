1c428905baf722a5bc40dd17f0d3eca1de32b22e
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 20:25:42 +0400

    prints the numbers, from 0 to 9, followed by a new line

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,8 @@
 /**
  * print_numbers - Prints the numbers from 0 to 9 followed by a new line.
  *
- * Description: This function prints all the numbers from 0 to 9 and then prints a new line.
+ * Description: This function prints all the numbers from 0 to 9 and
+ * then prints a new line.
  */
 
 void print_numbers(void)

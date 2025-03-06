718c7617f0721a23430d5c0cc8660bd680fe71f8
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 20:23:43 +0400

    prints the numbers, from 0 to 9, followed by a new line

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,7 +5,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
-int _putchar(int i);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);

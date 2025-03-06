60b8725f2f53d652bdc3bc79d64ad40dab3494e3
elshadyusifov <e.yusifov335@gmail.com>
2025-03-06 21:00:10 +0400

    I believe in numbers and signs without 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 {
 char numbers[] = "0123456789";
 int i = 0;
-while (numbers[i] != '\0')
+for (numbers[i] != '\0')
 if (numbers[3] != 2 && numbers[5] != 4)
 _putchar(numbers[i]);
 i++;

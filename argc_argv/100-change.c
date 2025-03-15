bffa5d6553f00f85f16c42fd1829c5487e06b339
elshadyusifov <e.yusifov335@gmail.com>
2025-03-15 23:29:51 +0000

    100 change coins

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 
 /**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ * Return: the number of characters printed
+ */
+int _putchar(char c)
+{
+    return (fwrite(1, &c, 1));
+}
+
+/**
  * main - prints the minimum number of coins to make change for an amount of money.
  * @argc: number of arguments passed to the program.
  * @argv: array of arguments passed to the program.
@@ -13,13 +23,19 @@ int main(int argc, char *argv[])
     int cents, coins = 0;
     if (argc != 2)
     {
-        _putchar("Error\n");
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
         return (1);
     }
     cents = atoi(argv[1]);
     if (cents < 0)
     {
-        _putchar("0\n");
+        _putchar('0');
+        _putchar('\n');
         return (0);
     }
     int coin_values[] = {25, 10, 5, 2, 1};
@@ -31,6 +47,24 @@ int main(int argc, char *argv[])
             cents %= coin_values[i];
         }
     }
-    _putchar("%d\n", coins);
+    char buffer[10];
+    int i = 0;
+    if (coins == 0)
+    {
+	    _putchar('0');
+    }
+    else
+    {
+	    while (coins > 0)
+	{
+		buffer[i++] = (coins % 10) + '0';
+	    coins /= 10;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		_putchar(buffer[j]);
+	}
+    }
+    _putchar('\n');
     return (0);
 }

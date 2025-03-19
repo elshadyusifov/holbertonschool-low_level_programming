12df67bac3ade5fa1f1e91f1c99d64d32dd0db1b
elshadyusifov <e.yusifov335@gmail.com>
2025-03-19 22:00:50 +0400

    first malloc task we want do

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * create_array - is main funcsion
+ * @size: give your argument
+ * @c: give your character
+ * return: 0 always successful
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if(size == 0)
+	{
+		return (NULL);
+	}
+	char *arr = malloc(size * sizeof(char)):
+		if(arr == NULL)
+	{
+		return (NULL);
+	}
+	for(unsigned int i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
+int main()
+{
+	unsigned int size = 98;
+	char c = 'H';
+	char *arr = create_array(size, c);
+	if(arr == NULL)
+	{
+		return (1);
+	}
+	for(unsigned int i = 0; i < size; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	free(arr);
+	return (0);
+}

caa21bd0a42e31ccef79320d22697c153119b635
elshadyusifov <e.yusifov335@gmail.com>
2025-03-20 17:24:21 +0400

    first malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,11 +11,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *arr = (char *)malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
 		if (arr == NULL)
 	{
 		return (NULL);
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
